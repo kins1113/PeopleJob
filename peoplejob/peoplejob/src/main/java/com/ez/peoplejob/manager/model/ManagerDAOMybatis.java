22cadc63264805d7252e541a19f99139282eb1b4
koh-9 <koh-9@DESKTOP-FCME4T4>
2019-07-01 21:07:46 +0900

    관리자 로그인 처리 	by-옥환

---
+++ peoplejob/peoplejob/src/main/java/com/ez/peoplejob/manager/model/ManagerDAOMybatis.java
@@ -0,0 +1,39 @@
+package com.ez.peoplejob.manager.model;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ManagerDAOMybatis implements ManagerDAO {
+	private String namespace="config.mybatis.mapper.oracle.manager.";
+	@Autowired private SqlSessionTemplate sqlSession;
+	
+	public String selectPwdById(String adminid) {
+		return sqlSession.selectOne(namespace+"loginCheck",adminid);
+	}
+	public List<ManagerVO> selectManagerAll(){
+		return sqlSession.selectList(namespace+"selectManagerAll");
+	}
+	public int insertManager(ManagerVO managerVo) {
+		return sqlSession.insert(namespace+"insertManager",managerVo);
+	}
+	
+	public int delectManager(int adminCode) {
+		return sqlSession.update(namespace+"delectManager",adminCode);
+	}
+	public String selectAuthorityById(String adminid) {
+		return sqlSession.selectOne(namespace+"selectAuthorityById",adminid);
+	}
+	public int selectIdChk(String adminid) {
+		return sqlSession.selectOne(namespace+"selectIdChk",adminid);
+	}
+	public ManagerVO selectById(int adminCode) {
+		return sqlSession.selectOne(namespace+"selectById", adminCode);
+	}
+	public int updateManager(ManagerVO managerVo) {
+		return sqlSession.update(namespace+"updateManager", managerVo);
+	}
+}
